60a35c81deb2c3cd986b324e925d724f31b88a64
unknown <febomtempo@hotmail.com>
2023-02-09 04:27:37 -0300

    feat: implement User model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('Event', userSchema);
+
+module.exports = User;
