7131f6f0505815cfb4b73867625700acd5bdf026
unknown <febomtempo@hotmail.com>
2023-02-09 05:42:18 -0300

    chore: add cors dependency

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const db = require('../database/connect');
 const eventRouter = require('../routes/eventRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 db();
